be0cc635c24df3b534a25f145bdbf073aa9b4be4
ucasvieira <lucas.souza1012@etec.sp.gov.br>
2022-04-05 12:21:07 -0300

    Versão 2.0

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
 require_once("pessoa.class.php");
+require_once("empresa.class.php");
 
 $pessoa = new Pessoa();
 $pessoa->setNome("Lucas Vieira de Souza");
 $pessoa->setEndereco("\nRua Manuel Vila D'alba");
 echo $pessoa->getNome();
+
+$empresa = new Empresa();
+$empresa->setRazaoSocial("Escola XPTO LTDA");
+echo $empresa->getRazaoSocial();
+
 
 ?>
